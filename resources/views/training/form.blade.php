4bffcebd347119049906a7e75fbdaf0e190e57fb
IgorPro17 <igor1799@mail.ru>
2021-05-04 11:39:35 +0300

    time

--- resources/views/training/form.blade.php
+++ resources/views/training/form.blade.php
@@ -32,7 +32,7 @@
         </div>
         <div class="field">
             <label class="label">Дата и время:</label>
-            <input name="time" type="datetime-local" min="07:00" max="23:00" step="3600000">
+            <input name="time" type="time" min="07:00" max="23:00" step="3600000">
             @if ($errors->has('time'))
                 <span class="is-danger">{{ $errors->first('time') }}</span>
             @endif
